aeab226c5c4c7c1692f97dbbdbec109dcfeb9da5
Matteus Colins <matteus.colins@ceuma.br>
2020-06-27 20:22:52 -0300

    ajustes no metodo de consulta e edição

--- src/main/java/br/com/ithappens/carrinho/ProdutoNegocio.java
+++ src/main/java/br/com/ithappens/carrinho/ProdutoNegocio.java
@@ -57,7 +57,7 @@ public class ProdutoNegocio {
             System.out.print("Informe o código do produto: ");
             long codigo = scanner1.nextLong();
             Scanner scanner2 = new Scanner(System.in);
-            System.out.print("Informe o código do produto: ");
+            System.out.print("Informe a descrição do produto: ");
             String descricao = scanner2.nextLine();
             if(produtos.size() > 0){
                 for (Produto prod:produtos) {
@@ -104,19 +104,100 @@ public class ProdutoNegocio {
     }
 
     public static void consulta(){
-        System.out.println("PRODUTOS CADASTRADOS");
-        System.out.println("|COD|DESCRICAO|");
-        System.out.println("|---|----------------------------------------------|");
-        for (Produto produto: produtos) {
-            System.out.println("|" + produto.getCodigo()+"  | "+produto.getDescricao().toUpperCase());
+        try {
+            if(produtos.size() > 0){
+                System.out.println("PRODUTOS CADASTRADOS");
+                System.out.println("|COD|DESCRICAO|");
+                System.out.println("|---|----------------------------------------------|");
+                for (Produto produto: produtos) {
+                    System.out.println("|" + produto.getCodigo()+"  | "+produto.getDescricao().toUpperCase());
+                }
+                int op = 0;
+                do {
+                    System.out.println("Deseja retornar ao menu de produtos?");
+                    System.out.println("1 - Sim");
+                    System.out.println("2 - Não");
+                    Scanner scanner = new Scanner(System.in);
+                    op = scanner.nextInt();
+                } while (op > 2);
+                if(op == 1){
+                    menu();
+                } else if (op == 2){
+                    App.menu();
+                }
+            } else {
+                System.out.println("ZERO PRODUTOS CADASTRADOS!");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
+
     }
 
     public static void edita(){
+        long codigoProduto = 0;
+        String descricao = null;
+        try {
+            if(produtos.size() > 0){
+                System.out.println("PRODUTOS CADASTRADOS");
+                System.out.println("|COD|DESCRICAO|");
+                System.out.println("|---|----------------------------------------------|");
+                for (Produto produto: produtos) {
+                    System.out.println("|" + produto.getCodigo()+"  | "+produto.getDescricao().toUpperCase());
+                }
+                System.out.println("|--------------------------------------------------|");
+                System.out.print("Informe o código do produto que deseja editar: ");
+                Scanner scanner = new Scanner(System.in);
+                codigoProduto = scanner.nextLong();
+                for (Produto produto: produtos) {
+                    if(produto.getCodigo() == codigoProduto){
+                        System.out.print("Informe a nova descrição do produto: ");
+                        Scanner scanner1 = new Scanner(System.in);
+                        descricao = scanner1.nextLine();
+                        produto.setDescricao(descricao);
+                        consulta();
+                    } else {
+                        int op = 0;
+                        System.out.println("Não foi possível encontrar o produto com o código informado!");
+                        do{
+                            System.out.println("DESEJA INFORMAR O CÓDIGO NOVAMENTE?");
+                            System.out.println("1 - Sim");
+                            System.out.println("2 - Não");
+                            Scanner scanner2 = new Scanner(System.in);
+                            op = scanner2.nextInt();
+                        }while (op > 2);
+                        if(op == 1){
+                            edita();
+                        } else {
+                            menu();
+                        }
+                    }
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
     public static void delete(){
+        long codigoProduto = 0;
+        try {
+            if(produtos.size() > 0){
+                System.out.print("Informe o código do produto que deseja deletar: ");
+                Scanner scanner = new Scanner(System.in);
+                codigoProduto = scanner.nextLong();
+                for (Produto produto:produtos) {
+                    if(produto.getCodigo().equals(codigoProduto)){
+                        produtos.remove(produto);
+                    }
+                }
+            } else {
+                System.out.println("Sem produtos para deletar");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 
